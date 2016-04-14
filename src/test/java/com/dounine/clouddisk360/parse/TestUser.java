0792eb12742248e42e6664ddbffd80cf34dff780
huanghuanlai <102535481@qq.com>
2016-04-14 15:06:37 +0800

    add TestUser class

---
+++ src/test/java/com/dounine/clouddisk360/parse/TestUser.java
@@ -0,0 +1,15 @@
+package com.dounine.clouddisk360.parse;
+
+import com.dounine.clouddisk360.parser.deserializer.login.LoginUserToken;
+import com.dounine.clouddisk360.store.BasePathCommon;
+
+public final class TestUser {
+	
+	static{
+		BasePathCommon.BASE_PATH = "/Users/huanghuanlai/Desktop/clouddisk/";
+	}
+
+	public static final LoginUserToken LOGIN_USER_TOKEN = new LoginUserToken("102535481@qq.com",
+			"xxxxx");
+
+}
